10a518706a96c3a05528bccdc677a7c3173db0cb
Arif Hidayat <arifhidayat1010@gmail.com>
2018-11-17 21:13:22 +0700

    Update nomer6.sql

--- 6/nomer6.sql
+++ 6/nomer6.sql
@@ -1,5 +1,189 @@
-SELECT product_categories.id, product_categories.name,
-COUNT(products.category_id) as Jumlah_product
-FROM products INNER JOIN
-product_categories ON products.category_id = product_categories.id
-GROUP BY product_categories.id
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 17 Nov 2018 pada 14.59
+-- Versi server: 10.1.35-MariaDB
+-- Versi PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arkademy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `comment`
+--
+
+CREATE TABLE `comment` (
+  `id_comment` int(255) NOT NULL,
+  `id_post` int(255) NOT NULL,
+  `content` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `join_product`
+--
+
+CREATE TABLE `join_product` (
+  `id` int(25) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `jumlah` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `category_id` varchar(100) NOT NULL,
+  `created_date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category_id`, `created_date`) VALUES
+(1, 'Sabun Wangi', '1', '2014-12-22 12:45:34'),
+(2, 'Minuman Cola', '2', '2014-12-22 12:45:36'),
+(3, 'Prenagon Gold', '3', '2014-12-22 12:46:34'),
+(4, 'Aquaa', '2', '2014-12-22 12:47:34'),
+(5, 'The Botol', '2', '2014-12-22 12:48:34'),
+(6, 'Shampoo', '1', '2014-12-22 12:48:40');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product_categories`
+--
+
+CREATE TABLE `product_categories` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `product_categories`
+--
+
+INSERT INTO `product_categories` (`id`, `name`, `created_date`) VALUES
+(1, 'Peralatan Mandi', '2014-11-22 12:45:34'),
+(2, 'Minuman Kemasan', '2014-11-22 12:46:34'),
+(3, 'Produk Susu', '2014-11-22 12:47:34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` enum('administrator','operator','member','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'Ganie', 'Gani', '42b438a4533d22d6473647d3b5f81f75', 'administrator'),
+(2, 'James', 'Bayu', 'a430e06de5ce438d499c2e4063d60fd6', 'operator'),
+(3, 'xyz', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'member');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id_comment`);
+
+--
+-- Indeks untuk tabel `join_product`
+--
+ALTER TABLE `join_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `product_categories`
+--
+ALTER TABLE `product_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id_comment` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `join_product`
+--
+ALTER TABLE `join_product`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `product_categories`
+--
+ALTER TABLE `product_categories`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
